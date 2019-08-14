c3a19e05cae71613f4cf142f62dfa1109da7a44f
Haley <haleylopez95@gmail.com>
2019-08-13 19:36:02 -0700

    Remove default example

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -2,141 +2,17 @@
  * HomePage
  *
  * This is the first thing users see of our App, at the '/' route
+ *
  */
 
-import React, { useEffect, memo } from 'react';
-import PropTypes from 'prop-types';
-import { Helmet } from 'react-helmet';
+import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { createStructuredSelector } from 'reselect';
-
-import { useInjectReducer } from 'utils/injectReducer';
-import { useInjectSaga } from 'utils/injectSaga';
-import {
-  makeSelectRepos,
-  makeSelectLoading,
-  makeSelectError,
-} from 'containers/App/selectors';
-import H2 from 'components/H2';
-import ReposList from 'components/ReposList';
-import CenteredSection from './CenteredSection';
-import Form from './Form';
-import Input from './Input';
-import Section from './Section';
 import messages from './messages';
-import { loadRepos } from '../App/actions';
-import { changeUsername } from './actions';
-import { makeSelectUsername } from './selectors';
-import reducer from './reducer';
-import saga from './saga';
 
-const key = 'home';
-
-export function HomePage({
-  username,
-  term,
-  loading,
-  error,
-  repos,
-  onSubmitForm,
-  onChangeUsername,
-}) {
-  useInjectReducer({ key, reducer });
-  useInjectSaga({ key, saga });
-
-  useEffect(() => {
-    // When initial state username is not null, submit the form to load repos
-    if (username && username.trim().length > 0) onSubmitForm();
-  }, []);
-
-  const reposListProps = {
-    loading,
-    error,
-    repos,
-  };
-
+export default function HomePage() {
   return (
-    <article>
-      <Helmet>
-        <title>Eateries</title>
-        <meta name="description" content="A destination for popular eats" />
-      </Helmet>
-      <div>
-        <CenteredSection>
-          <H2>
-            <FormattedMessage {...messages.description} />
-          </H2>
-          <p>
-            <FormattedMessage {...messages.startSearch} />
-          </p>
-        </CenteredSection>
-        <Section>
-          <Form onSubmit={onSubmitForm}>
-            <label htmlFor="term">
-              <FormattedMessage {...messages.term} />
-              <Input
-                id="term"
-                type="text"
-                placeholder="spas, tacos, barbers"
-                value={term}
-                onChange={onChangeUsername}
-              />
-            </label>
-          </Form>
-          <Form onSubmit={onSubmitForm}>
-            <label htmlFor="username">
-              <FormattedMessage {...messages.location} />
-              <Input
-                id="username"
-                type="text"
-                placeholder="Oakland, CA"
-                value={username}
-                onChange={onChangeUsername}
-              />
-            </label>
-          </Form>
-          <ReposList {...reposListProps} />
-        </Section>
-      </div>
-    </article>
+    <h1>
+      <FormattedMessage {...messages.header} />
+    </h1>
   );
 }
-
-HomePage.propTypes = {
-  loading: PropTypes.bool,
-  error: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
-  repos: PropTypes.oneOfType([PropTypes.array, PropTypes.bool]),
-  onSubmitForm: PropTypes.func,
-  username: PropTypes.string,
-  term: PropTypes.string,
-  onChangeUsername: PropTypes.func,
-};
-
-const mapStateToProps = createStructuredSelector({
-  repos: makeSelectRepos(),
-  username: makeSelectUsername(),
-  loading: makeSelectLoading(),
-  error: makeSelectError(),
-});
-
-export function mapDispatchToProps(dispatch) {
-  return {
-    onChangeUsername: evt => dispatch(changeUsername(evt.target.value)),
-    onSubmitForm: evt => {
-      if (evt !== undefined && evt.preventDefault) evt.preventDefault();
-      dispatch(loadRepos());
-    },
-  };
-}
-
-const withConnect = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-);
-
-export default compose(
-  withConnect,
-  memo,
-)(HomePage);
--- app/containers/HomePage/messages.js
+++ app/containers/HomePage/messages.js
@@ -1,28 +1,15 @@
 /*
  * HomePage Messages
  *
- * This contains all the text for the HomePage component.
+ * This contains all the text for the HomePage container.
  */
 import { defineMessages } from 'react-intl';
 
-export const scope = 'boilerplate.containers.HomePage';
+export const scope = 'app.containers.HomePage';
 
 export default defineMessages({
-  description: {
-    id: `${scope}.description`,
-    defaultMessage: 'Your go to location for all of the finest eats',
-  },
-  startSearch: {
-    id: `${scope}.startSearch`,
-    defaultMessage:
-      "To begin, enter what you are looking for and where you'd like to find it below.",
-  },
-  term: {
-    id: `${scope}.term`,
-    defaultMessage: 'Find: ',
-  },
-  location: {
-    id: `${scope}.location`,
-    defaultMessage: 'Location: ',
+  header: {
+    id: `${scope}.header`,
+    defaultMessage: 'This is the HomePage container!',
   },
 });
